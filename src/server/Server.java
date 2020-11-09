230d7ca5b1b85455fca494deb6391d64e92eab9e
BigKahuna7385 <danielburger2@gmx.de>
2020-11-09 23:32:46 +0100

    removed comments

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,46 +1,15 @@
 package server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
-//    private ServerSocket serverSocket;
-//    private Socket clientSocket;
-//    private PrintWriter out;
-//    private BufferedReader in;
-//
-//    public void start(int port) throws IOException {
-//        serverSocket = new ServerSocket(port);
-//        clientSocket = serverSocket.accept();
-//        System.out.println(clientSocket.toString());
-//        out = new PrintWriter(clientSocket.getOutputStream(), true);
-//        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//        String greeting = in.readLine();
-//        if ("hello server".equals(greeting)) {
-//            out.println("hello client");
-//        }
-//        else {
-//            out.println("unrecognised greeting");
-//        }
-//    }
-//
-//    public void stop() throws IOException {
-//        in.close();
-//        out.close();
-//        clientSocket.close();
-//        serverSocket.close();
-//    }
 
     private List<Room> roomList;
     private LandingRoom landingRoom;
 
-    Server(){
+    Server() {
         roomList = new ArrayList<>();
         try {
             SocketThread socketThread = new SocketThread(9999, this);
@@ -52,7 +21,7 @@ public class Server {
         this.landingRoom.start();
     }
 
-    public void addRoom(String roomName){
+    public void addRoom(String roomName) {
         this.roomList.add(new Room(roomName));
     }
 
@@ -65,7 +34,7 @@ public class Server {
     }
 
     public static void main(String[] args) throws IOException {
-        Server server=new Server();
+        Server server = new Server();
 
     }
 }
